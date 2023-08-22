5817bffdea8924fb32884e6c918c28ed1bca0048
Davi Dantas <davi.soares02@gmail.com>
2023-08-22 10:53:11 -0300

    fully function async chat

--- chat/websocket/consumers.py
+++ chat/websocket/consumers.py
@@ -1,18 +1,32 @@
-import json
 
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        print('cheguei')
-        self.accept()
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        await self.accept()
 
-        self.send(text_data=json.dumps({"message": message}))
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    async def receive(self, text_data):
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": text_data}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        await self.send(text_data=message)
